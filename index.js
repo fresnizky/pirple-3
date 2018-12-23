04aa20ea10a298b4249a6f0e0f8890882eda0322
Federico Resnizky <federico.resnizky@intive-fdv.com>
2018-12-23 16:09:26 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+ * Primary file for API
+ * 
+ */
+
+ // Dependencies
+ const server = require('./lib/server');
+ const workers = require('./lib/workers');
+
+// Declare the app
+let app = {};
+
+// Init function
+app.init = () => {
+    // Start the server
+    server.init();
+    
+    // Start the workers
+    workers.init();
+};
+
+// Execute function
+app.init();
+
+
+// Export the app
+module.exports = app;
